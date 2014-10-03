9d5453c40167b1d347d31fa0213aab1a25aed8e9
alesresta <alessandro.resta@gmail.com>
2014-10-03 22:32:56 +0100

    add Euler1.java

---
+++ Euler1.java
@@ -0,0 +1,28 @@
+/*
+ * Multiples of 3 and 5
+
+ * Problem 1
+
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+ * The sum of these multiples is 23.
+ * 
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ * Expected result: 233168
+ */
+public class Euler1 {
+
+    public static void main(String[] args){
+        
+        int i = 0;
+        int sum = 0;
+        
+        while(i<1000){
+            if(i%3==0 || i%5==0 ){
+                sum += i;    
+            }
+            i++;
+        }    
+        System.out.println(sum);    
+    }
+}
