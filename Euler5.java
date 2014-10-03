5580f1fafde8f1d8fded0d3e6bfe262b43ef8b43
alesresta <alessandro.resta@gmail.com>
2014-10-03 23:11:36 +0100

    adding comments to Euler5

--- Euler5.java
+++ Euler5.java
@@ -16,12 +16,15 @@ public class Euler5 {
         int i = 1;
         int n = 2520;
         
+        /** Se o resto da divisão de n por i for igual a zero, incrementa o valor de i
+         e repete o ciclo até o valor de i igual a 20 */
         while (evalor == false){
             if(n%i==0){
                 i++;
                 if(i == 20){
                     evalor = true;
                 }
+            // caso o resto da divisão seja diferente de zero, incrementa o valor de n e reinicia o valor da variável i
             }else{
                 n++;
                 i = 1;
