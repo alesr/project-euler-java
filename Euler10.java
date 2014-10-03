48e1ecede382f2f197f8c0365b85c82575a409aa
alesresta <alessandro.resta@gmail.com>
2014-10-03 23:48:01 +0100

    adding comments to Euler10

--- Euler10.java
+++ Euler10.java
@@ -13,18 +13,26 @@ public class Euler10 {
     public static void main(String[] args) {
    
         long count = 0;
+
+        /** Para cada n entre 2 e 2 milhões, caso n seja primo, incremanta o valor de n à variável count */
         for (int n=2; n<2000000; n++) {
             if (primo(n) == true){           
-                count = count + n;
+                count += n;
             }
                               
         }
         System.out.println(count);
     }
     
+
+    /** @return true se n for primo */
     public static boolean primo(int n) {
+
         boolean eprimo = true;
         int i = 2;
+
+        /** Se o resto da divisão de n por i for resto zero, significa que o número tem mais divisores além dele mesmo (n). 
+        Caso contrário, incrementa-se o valor de i e repete-se o ciclo para todos os valores até a raíz quadrada de n */
         while ((eprimo == true) && (i <= (Math.sqrt(n)))) {
             if ((n % i) == 0)
                 eprimo = false;
