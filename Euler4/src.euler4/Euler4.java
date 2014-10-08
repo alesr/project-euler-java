374918f4ec87475dace93be94f3b00ca1791ef81
alesresta <alessandro.resta@gmail.com>
2014-10-08 10:19:24 +0100

    adding comments to Euler4

--- Euler4/src.euler4/Euler4.java
+++ Euler4/src.euler4/Euler4.java
@@ -1,3 +1,14 @@
+/*
+ * Largest palindrome product
+ * 
+ * Problem 4  
+ * 
+ * A palindromic number reads the same both ways.
+ * The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ * 
+ * Expected result: 906609
+ */
 public class Euler4 {
     
     public static void main(String[] args){
